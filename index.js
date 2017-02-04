bbaf024706b0c76fdd27c9873f0e2f41d3eddab0
wangjames <wang.james.y@gmail.com>
2017-02-04 02:31:31 +0000

    chat functionality added through socket.io

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+  
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
 
 http.listen(8080, function(){
   console.log('listening on *:8080');
 });
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+
+
