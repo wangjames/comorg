276da9d9f706dd849b2c661b5e5173ea55ae0214
wangjames <wang.james.y@gmail.com>
2017-02-04 02:00:48 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var app = require('express')();
+var http = require('http').Server(app);
+
+app.get('/', function(req, res){
+  res.send('<h1>Hello world</h1>');
+});
+
+http.listen(8080, function(){
+  console.log('listening on *:8080');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
