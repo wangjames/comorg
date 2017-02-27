861e6967d3f592cfa6c71381f409ee5d668e4c94
wangjames <wang.james.y@gmail.com>
2017-02-27 08:37:00 +0000

    got users and messages to work!

--- index.js
+++ index.js
@@ -13,12 +13,18 @@ app.get('*', function(req, res) {
 
 var users = []
 io.on('connection', function(socket){
-
+  
+  socket.emit('setUsers', users);
+  
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
     console.log(msg);
   });
   
+  socket.on('add user', function(user){
+    users.push(user)
+    io.emit('setUsers', users)
+  });
   
   socket.on('disconnect', function(){
     console.log('user disconnected');
